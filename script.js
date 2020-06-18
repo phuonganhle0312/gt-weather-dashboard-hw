297b6a0cde800af9d94b09ce238f4ccefdafbe06
Phuong Le <phuonganh.le0312@gmail.com>
2020-06-17 22:46:34 -0400

    made api calls into one function retrieved 5 day forcast data.

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-//API key
+//declared variables
 const apikey= "a8ffefde4c3d97d26c0c126fb6117891";
-//retrieve longitude latitude for current weather
-function currentWeather(){
-    navigator.geolocation.getCurrentPosition(function(position){
-        long=position.coords.longitude;
-        lat=position.coords.latitude;
-
-        let queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=" +  apikey;
-
+let city="";
+//2 API calls
+function APIcalls(){
+        //retrieve current weather and forecast data
+        let forecastUrl= "https://api.openweathermap.org/data/2.5/forecast?q="; 
+        let currentUrl="https://api.openweathermap.org/data/2.5/weather?q=";
+        let uvUrl="api.openweathermap.org/data/2.5/uvi?appid=" + apikey + "&lat="+ lat + "&lon="+ long;
+        queryurl = forecastUrl + city + apikey;
+        currentWeatherUrl = currentUrl + city + apikey;
+        
+        //CURRENT WEATHER
         $.ajax({
-            url: queryURL,
-            method: GET
+            url: currentWeatherURL,
+            method: "GET",
         })
-        //store data in response
-        .then(function(response){
+        
+        .then(function(response) {
             let icon = response.weather[0].icon;
             let iconurl= "http://openweathermap.org/img/w/" + icon + ".png";
             $("#cityName").text(response.name);
@@ -21,18 +24,42 @@ function currentWeather(){
             $(".humidity").text("Humidity: " + response.main.humidity + " %");
             $(".windspeed").text("Wind Speed: " + response.wind.speed + " MPH");
             $("#icon").attr("src",iconurl);
-            //repear with UV index
-            let queryURL="api.openweathermap.org/data/2.5/uvi?appid=" + apikey + "&lat="+ lat + "&lon="+ long;
+            //UV index
+            
             $.ajax({
-                url: queryURL,
-                method: GET
+                url: uvUrl,
+                method: "GET"
             })
-            .then(function(response){
-                $(".uvIndex").text("UV Index :" + response.uv.index);
+            .then(function(responseTwo){
+                $(".uvIndex").text("UV Index :" + responseTwo.uv.index);
+            
+            })
+            //Retrieve 5 day forecast data
+            $.ajax({
+                url:forecastUrl,
+                method: "GET"
+            })
+            .then(function(responseThree){
+                //icon data for forecats
+                let icon1= responseThree.list[4].weather[0].icon;
+                let icon1url= "http://openweathermap.org/img/w/" + icon1 + ".png";
 
-            }
-            )}
-        )}
-    )};
-    currentWeather ();
-    console.log(currentWeather)
+                let icon2= responseThree.list[4].weather[0].icon;
+                let icon2url= "http://openweathermap.org/img/w/" + icon2 + ".png";
+
+                let icon3= responseThree.list[4].weather[0].icon;
+                let icon3url= "http://openweathermap.org/img/w/" + icon3 + ".png";
+
+                let icon4= responseThree.list[4].weather[0].icon;
+                let icon4url= "http://openweathermap.org/img/w/" + icon4 + ".png";
+
+                let icon5= responseThree.list[4].weather[0].icon;
+                let icon5url= "http://openweathermap.org/img/w/" + icon5 + ".png";
+
+            })
+        });
+    
+    };
+    
+    APIcalls ();
+    console.log(APIcalls)
